2238c74ea5ff119e7fa65fd8bea7d6aba1f44e66
Ivan Sen <senivan1997@gmail.com>
2023-11-06 10:30:26 +0000

    Parse infrastructure set up

--- project2_task1_b.py
+++ project2_task1_b.py
@@ -1,2 +1,18 @@
-def replace_substring(substring:str, repl:str, filename:str) -> None:
+def replace_substring(substring:str, repl:str, filename:str, inplace:int) -> None:
     pass
+
+if __name__ == "__main__":
+    # import doctest
+    # print(doctest.testmod())
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog='task 1 subtask b',
+        description='replace substring in file'
+    )    
+    parser.add_argument('substring', type=str, help='substring to replace')
+    parser.add_argument('repl', type=str, help='replacement string')
+    parser.add_argument('filename', type=str, help='file to modify')
+    parser.add_argument('--inplace',action='store_const', const=1, help='modify file in place')
+    args = parser.parse_args()
+    # print(args)
+    replace_substring(args.substring, args.repl, args.filename, args.inplace)
