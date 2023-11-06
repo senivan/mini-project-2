f59a8de2ab7b615777742eb940e9fcc9ac4b406f
Ivan Sen <senivan1997@gmail.com>
2023-11-06 15:11:35 +0000

    find_same func is done

---
+++ project2_task1_e.py
@@ -0,0 +1,29 @@
+"""Mini-project 2 task 1 subtask e by Sen Ivan"""
+import os
+def find_same(path1:str, path2:str, res_path:str) -> None:
+    if not os.path.isfile(res_path):
+        raise ValueError('Result file does not exist or is directory')
+    if not (os.path.isfile(path1) and os.path.isfile(path2)):
+        raise ValueError('One of the files does not exist or is directory')
+    with open(path1, 'r', encoding='utf-8') as file1, open(path2, 'r', encoding="utf-8")\
+ as file2, open(res_path, 'w+', encoding="utf-8") as res_file:
+        lines1 = file1.readlines()
+        lines2 = file2.readlines()
+        for line1 in lines1:
+            if line1 in lines2 and line1 not in res_file:
+                res_file.write(line1)
+if __name__ == "__main__":
+    # import doctest
+    # print(doctest.testmod())
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog='task 1 subtask e',
+        description='find same lines in files'
+    )
+
+    parser.add_argument('path1', type=str, help='first file to compare')
+    parser.add_argument('path2', type=str, help='second file to compare')
+    parser.add_argument('dst', type=str, help='file to write results to')
+
+    args = parser.parse_args()
+    find_same(args.path1, args.path2, args.dst)
